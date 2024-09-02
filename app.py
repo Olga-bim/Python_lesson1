d2fe6d2e82fdcfec03f6c3bbe912f584a50cf89b
Olga <olga.bimstart@gmail.com>
2024-09-02 10:35:35 +0300

    commit 5

--- app.py
+++ app.py
@@ -66,103 +66,110 @@
 # Если число делится только на 5, выводится "Buzz".
 # Если число не делится ни на 3, ни на 5, выводится само число.
 
-def fizz_buzz(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+# def fizz_buzz(n):
+#     for i in range(1, n + 1):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("FizzBuzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         elif i % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(i)
+
+# # Пример использования:
+# fizz_buzz(20)
+
+
+
+# def fizz_buzz(n):
+#     i = 1
+#     while i <= n:
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("FizzBuzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         elif i % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(i)
+#         i += 1  # Увеличиваем значение i на 1 в каждом цикле
 
 # Пример использования:
-fizz_buzz(20)
+# fizz_buzz(15)
+
+import json
+import os
+
+contacts = []
+
+file_path = "contacts.json"
+
+def getUserData(contacts):
+    userName = input("Enter your name?")
+    email = input("email?")
+    contacts.append({"user": userName, "email": email})
+
+def clearScreen():
+      os.system('cls' if os.name == 'nt' else 'clear')
+
+def showContacts(contacts):
+    print("All contacts")
+    print(contacts)
 
 
+def searchContact(contacts):
+    search_name = input("Enter the name to search: ")
+    found = False
+    for contact in contacts:
+        if search_name.lower() in contact['user'].lower():
+            print(f"Name: {contact['user']}, Email: {contact['email']}")
+            found = True
+    if not found:
+        print("No contact found with that name.")
 
-def fizz_buzz(n):
-    i = 1
-    while i <= n:
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
-        i += 1  # Увеличиваем значение i на 1 в каждом цикле
+def exit_app(contacts):
+    with open('contacts.json', 'w') as file:
+        json.dump(contacts, file, indent=4)        
 
-# Пример использования:
-fizz_buzz(15)
-
-# import json
-# import os
-
-# contacts = []
-
-# file_path = "contacts.json"
-
-# def getUserData(contacts):
-#     userName = input("Enter your name?")
-#     email = input("email?")
-#     contacts.append({"user": userName, "email": email})
-
-# def clearScreen():
-#       os.system('cls' if os.name == 'nt' else 'clear')
-
-# def showContacts(contacts):
-#     print("All contacts")
-#     print(contacts)
-
-
-# def searchContact(contacts):
-#     search_name = input("Enter the name to search: ")
-#     found = False
-#     for contact in contacts:
-#         if search_name.lower() in contact['user'].lower():
-#             print(f"Name: {contact['user']}, Email: {contact['email']}")
-#             found = True
-#     if not found:
-#         print("No contact found with that name.")
-
-# def main():
+def main():
      
-#      with open('contacts.json', 'r') as file:
-#         contacts = json.load(file)
-#         print(contacts)
+     with open('contacts.json', 'r') as file:
+        contacts = json.load(file)
+        print(contacts)
 
-#         while True:
+        while True:
   
-#             print ("1 - Add new contact")
-#             print ("2 - del a contact")
-#             print ("3 - edit contact")
-#             print ("4 - show all contact")
-#             print ("5 - clear")
-#             print ("6 - search contact")
-#             print ("x - exit")
+            print ("1 - Add new contact")
+            print ("2 - del a contact")
+            print ("3 - edit contact")
+            print ("4 - show all contact")
+            print ("5 - clear")
+            print ("6 - search contact")
+            print ("x - exit")
 
-#             userSelection = input("selection?")
-#             if userSelection == "x": 
-#                 with open('contacts.json', 'w') as file:
-#                     json.dump(contacts, file, indent=4)
-#                 return
-#             if userSelection == "1": 
-#                 getUserData(contacts)
-#             if userSelection == "2": 
-#                 # delete
-#                 pass
-#             if userSelection == "3": print("edit")
-#             if userSelection == "5": 
-#                 clearScreen()
-#             if userSelection == "4": 
-#                 showContacts(contacts)
-#             if userSelection == "6":
-#                 searchContact(contacts)
+            userSelection = input("selection?")
 
-# if __name__== "__main__":
-#         main()
+            if userSelection == "x": 
+                 exit_app()(contacts)
+                 return
+            if userSelection == "1": 
+                getUserData(contacts)
+            if userSelection == "2": 
+                # delete
+                pass
+            if userSelection == "3": 
+                print("edit")
+            if userSelection == "5": 
+                clearScreen()
+            if userSelection == "4": 
+                showContacts(contacts)
+            if userSelection == "6":
+                searchContact(contacts)
+
+
+
+if __name__== "__main__":
+        main()
 
 
